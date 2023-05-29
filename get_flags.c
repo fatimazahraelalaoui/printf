57dfd25b8e4b6933224f4c27c7dc97c093ef2ab0
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-05-29 15:25:04 -0700

    print

--- get_flags.c
+++ get_flags.c
@@ -10,25 +10,26 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int x, curr;
+	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr = *i + 1; format[curr] != '\0'; curr++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (x = 0; FLAGS_CH[x] != '\0'; x++)
-			if (format[curr] == FLAGS_CH[x])
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flags |= FLAGS_ARR[x];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
-		if (FLAGS_CH[x] == 0)
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
-	*i = curr - 1;
+	*i = curr_i - 1;
 
 	return (flags);
 }
+
